0823f4cbaa72576cd0f62cc97b9f6b67284cb279
Marcus Behel <marcus.behel@mindspring.com>
2023-03-07 22:34:54 -0500

    Add initial script and instructions

---
+++ README.md
@@ -0,0 +1,82 @@
+# OpenCV Build Scripts
+
+## linux-amd64-pc
+
+- Build on a Linux system (assumed to be `amd64` / `x86_64`)
+- Setup a chroot (to build on older distro for glibc compatibility)
+    ```sh
+    # Use different package manager if not debian based system
+    sudo apt install schroot debootstrap
+
+    # Make chroot
+    sudo debootstrap --variant=buildd --arch=amd64 bionic /srv/chroot/bionic http://archive.ubuntu.com/ubuntu/
+    ```
+- Write chroot config file
+    ```conf
+    [bionic-amd64]
+    description=Ubuntu 18.04 (amd64)
+    type=directory
+    directory=/srv/chroot/bionic
+    users=[YOUR_USERNAME]
+    root-groups=root
+    profile=desktop
+    personality=linux
+    ```
+- On more recent Ubuntu / Debian systems, `yescrypt` is used, but this is not supported in older systems (bionic included). Thus change `/etc/pam.d/common-password` by modifying `yescrypt` to be `sha512`. Failing to do so will prevent `sudo` from working in the chroot environment.
+- Enter chroot with `schroot -c bionic-amd64`
+- Run the build
+    ```sh
+    mkdir build
+    cd build
+    mkdir linux-amd64-pc
+    cd linux-amd64-pc
+    ../buildopencv.sh | tee build.log
+    ```
+- Wait for build then collect the artifacts from `build/` subdirectory (deb packages and tarball)
+
+
+
+## linux-arm64-pc
+
+- Build on a Linux system (assumed to be `amd64` / `x86_64`)
+- Setup a chroot (to build on older distro for glibc compatibility)
+    ```sh
+    # Use different package manager if not debian based system
+    sudo apt install schroot debootstrap qemu-user-static
+
+    # Make chroot
+    sudo debootstrap --variant=buildd --arch=arm64 bionic /srv/chroot/bionic http://archive.ubuntu.com/ubuntu/
+    ```
+- Write chroot config file
+    ```conf
+    [bionic-arm64]
+    description=Ubuntu 18.04 (arm64)
+    type=directory
+    directory=/srv/chroot/bionic
+    users=[YOUR_USERNAME]
+    root-groups=root
+    profile=desktop
+    personality=linux
+    ```
+- On more recent Ubuntu / Debian systems, `yescrypt` is used, but this is not supported in older systems (bionic included). Thus change `/etc/pam.d/common-password` by modifying `yescrypt` to be `sha512`. Failing to do so will prevent `sudo` from working in the chroot environment.
+- Enter chroot with `schroot -c bionic-arm64`
+- Run the build
+    ```sh
+    mkdir build
+    cd build
+    mkdir linux-arm64-pc
+    cd linux-arm64-pc
+    ../buildopencv.sh | tee build.log
+    ```
+- Wait for build then collect the artifacts from `build/` subdirectory (deb packages and tarball)
+
+
+
+## linux-arm64-jetsonnano
+
+TODO
+
+
+## windows-amd64-pc
+
+TODO
---
+++ buildopencv.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# NOTE: Install requirements for build before building
+# sudo apt install cmake build-essential openjdk-11-jdk python3 libpython3.$(python3 --version | sed -e 's/Python //g' | cut -d. -f2)-dev python3-numpy ant ffmpeg libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev wget unzip pkg-config libavutil-dev libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
+# Install python2.7 and libpython2.7 if building with python2
+# install cuda and cudnn if building with cuda
+# Install a jdk to build with java (eg default-jdk)
+# NOTE: Cross compiling is not recommended
+# NOTE: Compiling in a chroot for a different architecture using qemu-user-static should work
+
+# Exit on any error
+function exit_trap(){
+    ec=$?
+    if [ $ec -ne 0 ]; then
+        echo "\"${last_command}\" command failed with exit code $ec."
+    fi
+}
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap exit_trap EXIT
+
+
+# Configuration
+BUILD_THREADS=4                 # Use 1 on jetson nano or rpi
+CC=$(which gcc)                 # Use gcc-7 on jetson nano
+CXX=$(which g++)                # Use g++-7 on jetson nano
+VERSION=4.6.0
+CUDA=NO
+CUDA_ARCH_BIN=5.3               # 5.3 for jetson nano0
+CUDNN=NO
+PYTHON3=YES
+PYTHON2=NO
+JAVA=YES
+DEB_PKG=YES
+
+# Download and unzip sources
+
+if [ ! -d opencv-${VERSION}/ ];then
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/${VERSION}.zip
+    wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${VERSION}.zip
+    unzip opencv.zip
+    unzip opencv_contrib.zip
+fi
+
+# Build
+rm -rf build/
+mkdir -p build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release \
+    -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib-${VERSION}/modules \
+    -D WITH_CUDA=$CUDA \
+    -D CUDA_ARCH_BIN=$CUDA_ARCH_BIN \
+    -D CUDA_ARCH_PTX="" \
+    -D WITH_CUDNN=$CUDNN \
+    -D OPENCV_DNN_CUDA=$CUDNN \
+    -D ENABLE_FAST_MATH=ON \
+    -D CUDA_FAST_MATH=$CUDA \
+    -D WITH_CUBLAS=$CUDA \
+    -D WITH_LIBV4L=ON \
+    -D WITH_V4L=ON \
+    -D WITH_GSTREAMER=ON \
+    -D WITH_GSTREAMER_0_10=OFF \
+    -D WITH_FFMPEG=ON \
+    -D WITH_QT=OFF \
+    -D WITH_OPENGL=OFF \
+    -D BUILD_opencv_python2=$PYTHON2 \
+    -D BUILD_opencv_python3=$PYTHON3 \
+    -D BUILD_TESTS=OFF \
+    -D BUILD_PERF_TESTS=OFF \
+    -D CMAKE_C_COMPILER=$CC \
+    -D CMAKE_CXX_COMPILER=$CXX \
+    -D CPACK_BINARY_DEB=$DEB_PKG \
+    -D OPENCV_VCSVERSION=$VERSION \
+    -D ANT_EXECUTABLE:FILEPATH=$(realpath $(which ant)) \
+    -D CPACK_PACKAGING_INSTALL_PREFIX=/opt/opencv-${VERSION}/ \
+    ../opencv-${VERSION}
+make -j ${BUILD_THREADS}
+
+
+# Pacakge
+ORIG_LD_LIB=$(echo $LD_LIBRARY_PATH)
+export LD_LIBRARY_PATH=$(pwd)/lib
+cmake --build . --target package
+export LD_LIBRARY_PATH=$ORIG_LD_LIB
